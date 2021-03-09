eeb1ed8aed5b3d9573883783924d54d85b3b257d
jaimefd <skyjimmy7@gmail.com>
2021-03-09 18:21:53 +0100

    Esquema base AlgoritmoGenetico e Individuo

--- Practica1/src/algoritmoGenetico/AlgoritmoGenetico.java
+++ Practica1/src/algoritmoGenetico/AlgoritmoGenetico.java
@@ -1,5 +1,57 @@
 package algoritmoGenetico;
 
-public class AlgoritmoGenetico {
+import algoritmoGenetico.individuos.Individuo;
 
+public class AlgoritmoGenetico {
+	
+	private int tipoFuncion;
+	private int tamPoblacion;
+	private Individuo[] poblacion;
+	private double[] fitness;
+	private int maxGeneraciones;
+	private double probCruce;
+	private double probMutacion;
+	private int tamTorneo;
+	private Individuo elMejor;
+	private int posMejor;
+	private int genActual;
+
+	public AlgoritmoGenetico(int tipoFuncion, int tamPoblacion, int maxGeneraciones, double probCruce, 
+			double probMutacion, int tamTorneo) {
+		this.tipoFuncion = tipoFuncion;
+		this.tamPoblacion = tamPoblacion;
+		this.maxGeneraciones = maxGeneraciones;
+		this.probCruce = probCruce;
+		this.probMutacion = probMutacion;
+		this.tamTorneo = tamTorneo;
+	}
+
+	public void run() {
+		inicializar();
+		while(this.genActual < this.maxGeneraciones) {
+			// seleccion
+			
+			// cruce
+			
+			// mutacion
+			
+			evaluar();
+			generarGrafica();
+			
+			this.genActual++;
+		}
+	}
+	
+	public void inicializar() {
+		this.genActual = 1;
+	}
+	
+	public void evaluar() {
+		
+	}
+	
+	public void generarGrafica() {
+		
+	}
+	
 }
--- Practica1/src/algoritmoGenetico/individuos/Individuo.java
+++ Practica1/src/algoritmoGenetico/individuos/Individuo.java
@@ -1,5 +1,14 @@
 package algoritmoGenetico.individuos;
 
-public class Individuo {
+public abstract class Individuo<T> {
+	
+	T[] cromosoma;
+	int[] tamGenes;
+	double[] min;
+	double[] max;
+	
+	public abstract double getValor();
+	public abstract double getFitness();
+	public abstract int tamGen();
 
 }
