ed3b06098150f027f7a4e7de3320ecb40f2d052d
juancar99 <62304103+juancar99@users.noreply.github.com>
2021-03-15 18:24:20 +0100

    Update Generacion.java

--- Practica1/src/algoritmoGenetico/individuos/Generacion.java
+++ Practica1/src/algoritmoGenetico/individuos/Generacion.java
@@ -8,10 +8,13 @@ public class Generacion {
 	private ArrayList<Double> puntuaciones;  //Array con las puntuaciones de cada individuo
 	private ArrayList<Double> puntAcu;  //Array con la puntuacion acumulada de cada individuo
 	private int tamPobl;
+	private int funcion;
 
-	private double media;  //media de la generacion
-	private Individuo elMejor;  //el mejor individuo de la generacion
-	private Individuo elPeor;  //el peor individuo de la generacion
+	private double media;  //media de la generacio para la grafica!
+	private Individuo elMejor;  //el mejor individuo de la generacion para la grafica!
+	private Individuo elPeor;  //el peor individuo de la generacion para la grafica!
+	private int pos_mejor_fitness;
+	private int pos_peor_fitness;
 	
 	
 	public Generacion(int tamañoPobl,int funcion,int numVariables4) {
@@ -19,6 +22,8 @@ public class Generacion {
 		poblacion = new ArrayList<Individuo<Boolean>>();
 		this.tamPobl = tamañoPobl;
 		this.puntuaciones = new ArrayList<Double>();
+		this.funcion=funcion;
+	
 		
 		if(funcion==1) {
 			for(int i = 0; i < tamPobl; i++)
@@ -53,7 +58,69 @@ public class Generacion {
 	}
 	
 	
-	public void evaluarPoblacion() {}
+	public void evaluarPoblacion() {
+		double puntAcum=0; //puntuacion acumulada
+		double fitnessTotal=0; //fitness total
+		double fitnessActual=0; //fitness del individuo actual
+		
+		if(funcion==1) {  //hay que diferenciar la funcion 1 de las demas
+			double mejorFitness=Double.MIN_VALUE; //mejor fitness de la generacion
+			double peorFitness=Double.MAX_VALUE;
+			
+			for(int i=0;i<this.tamPobl;i++) {
+				fitnessActual=this.poblacion.get(i).getFitness();
+				fitnessTotal+=fitnessActual;
+				if(fitnessActual>mejorFitness) {
+					mejorFitness=fitnessActual;
+					pos_mejor_fitness=i;
+					elMejor=this.poblacion.get(pos_mejor_fitness);
+				}
+				
+				if(fitnessActual<peorFitness) {
+					peorFitness=fitnessActual;
+					pos_peor_fitness=i;
+					elPeor=this.poblacion.get(pos_peor_fitness);
+				}
+			}
+		}
+		else {
+			double mejorFitness=Double.MAX_VALUE;
+			double peorFitness=Double.MIN_VALUE;
+			
+			for(int i=0;i<this.tamPobl;i++) {
+				fitnessActual=this.poblacion.get(i).getFitness();
+				fitnessTotal+=fitnessActual;
+				if(fitnessActual<mejorFitness) {
+					mejorFitness=fitnessActual;
+					pos_mejor_fitness=i;
+					elMejor=this.poblacion.get(pos_mejor_fitness);
+				}
+				
+				if(fitnessActual>peorFitness) {
+					peorFitness=fitnessActual;
+					pos_peor_fitness=i;
+					elPeor=this.poblacion.get(pos_peor_fitness);
+				}
+			}
+	
+		}
+		
+		for(int i=0;i<this.tamPobl;i++) {
+			this.puntuaciones.add(this.poblacion.get(i).getFitness()/fitnessTotal); //añado al array de puntuaciones la puntuacion del individuo
+			this.puntAcu.add(this.puntuaciones.get(i) + puntAcum); //añado al array de puntAcu la puntuacion acumulada del individuo
+			puntAcum+=this.puntuaciones.get(i); //sumo a la puntuacion acumulada la del indiv actual
+		}
+		
+		this.media=fitnessTotal/this.tamPobl;
+		
+		if(this.funcion==1) {
+			desplazamiento_maximizar();
+		}
+		else {
+			desplazamiento_minimizar();
+		}
+		
+	}
 	
 	public void seleccion() {}
 	
