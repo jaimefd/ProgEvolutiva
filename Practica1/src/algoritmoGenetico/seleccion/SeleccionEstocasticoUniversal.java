6f31d2d5cb545b75a14a52d4f3051d271fd3ed8d
juancar99 <62304103+juancar99@users.noreply.github.com>
2021-03-19 19:26:40 +0100

    estocastico bien

--- Practica1/src/algoritmoGenetico/seleccion/SeleccionEstocasticoUniversal.java
+++ Practica1/src/algoritmoGenetico/seleccion/SeleccionEstocasticoUniversal.java
@@ -9,12 +9,15 @@ public class SeleccionEstocasticoUniversal {
 	public static ArrayList<Individuo<Boolean>> seleccion(ArrayList<Individuo<Boolean>> poblacion,
 			ArrayList<Double> puntAcu, int tamPobl,ArrayList<Individuo<Boolean>> nuevaPobl) {
 		
-		double prob = Math.random()/tamPobl;
+		double tamaño=tamPobl;
+		double prob = Math.random()/tamaño;
 		int posi = 0;
 		for (int i = 0; i < tamPobl; i++) {
-			while (prob > puntAcu.get(posi) && posi < tamPobl - 1) posi++;
+			while (prob > puntAcu.get(posi) && posi < tamPobl - 1) {
+				posi++;
+			}
 			nuevaPobl.get(i).setCromosoma(poblacion.get(posi).getCromosoma()); 
-			prob += 1/tamPobl;
+			prob+= 1/tamaño;
 		}
 		return nuevaPobl;
 	}
