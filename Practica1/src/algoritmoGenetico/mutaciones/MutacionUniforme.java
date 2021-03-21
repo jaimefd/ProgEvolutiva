b3f4b70931e68092db546216a9adb03960e96a83
jaimefd <skyjimmy7@gmail.com>
2021-03-21 19:32:24 +0100

    Mutación uniforme

--- Practica1/src/algoritmoGenetico/mutaciones/MutacionUniforme.java
+++ Practica1/src/algoritmoGenetico/mutaciones/MutacionUniforme.java
@@ -9,8 +9,22 @@ public class MutacionUniforme {
 	static ArrayList<Individuo> mutar(int tipo, ArrayList<Individuo> poblacion,
 			double probMutacion,int tamPobl) {
 		
-		ArrayList<Individuo> nuevaPobl =new ArrayList<Individuo>();
+		for(int i=0;i<tamPobl;i++) {
+			int tamañoCromosoma =poblacion.get(i).getTamTotal();
+			Double[] aux=new Double[tamañoCromosoma]; //creamos un array auxiliar
+			aux=(Double[]) poblacion.get(i).getCromosoma(); //lo igualamos al cromosoma del individuo ¡puede que haya que usar .clone!
+			
+			for(int j=0;j<tamañoCromosoma;j++) {  
+				double rand= Math.random();
+				if(rand<probMutacion) {
+					double auxiliar=poblacion.get(i).getMax(j) +poblacion.get(i).getMin(j); 
+					rand*=auxiliar; //saca un numero aleatorio entre los valores permitidos
+					aux[j] = auxiliar; 
+				}
+			}
+			poblacion.get(i).setCromosoma(aux);
+		}
 		
-		return nuevaPobl;
+		return poblacion;
 	}
 }
