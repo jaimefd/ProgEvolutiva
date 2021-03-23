a3366611a53f76fe010bb515eac509eb1518dc88
juancar99 <62304103+juancar99@users.noreply.github.com>
2021-03-23 13:17:56 +0100

    Update Main.java

--- Practica1/src/main/Main.java
+++ Practica1/src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//Declaramos el panel donde se mostraran las opciones y se pondra en marcha el AG
-		Panel panel= new Panel();		
+		//Panel panel= new Panel();		
 	}
 
 }
