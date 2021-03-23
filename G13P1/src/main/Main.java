948b01ee2646304a1182aa8498fce9b6de4ea135
juancar99 <62304103+juancar99@users.noreply.github.com>
2021-03-23 14:03:16 +0100

    Update Main.java

--- G13P1/src/main/Main.java
+++ G13P1/src/main/Main.java
@@ -5,10 +5,10 @@ import gui.Panel;
 
 public class Main {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		
 		//Declaramos el panel donde se mostraran las opciones y se pondra en marcha el AG
 		//Panel panel= new Panel();		
-	}
+	}*/
 
 }
