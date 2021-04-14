21c57f88c4a275417f3185f3db444074d6eb1155
juancar99 <62304103+juancar99@users.noreply.github.com>
2021-04-14 13:23:22 +0200

    Create FactoriaMutacion.java

---
+++ G13P2/src/algoritmoGenetico/mutaciones/FactoriaMutacion.java
@@ -0,0 +1,26 @@
+package algoritmoGenetico.mutaciones;
+
+import java.util.ArrayList;
+import algoritmoGenetico.individuos.Individuo;
+
+
+public class FactoriaMutacion {
+	public static ArrayList<Individuo> getTipoMutacion (int tipo, ArrayList<Individuo> poblacion,
+			double probMutacion,int tamPobl) {
+		switch (tipo) {
+			case 1:
+				return insercion.mutar(tipo, poblacion, probMutacion,tamPobl);			
+			case 2:
+			
+				return intercambio.mutar(tipo, poblacion, probMutacion,tamPobl);
+			case 3:
+				return inversion.mutar(tipo, poblacion, probMutacion,tamPobl);
+			case 4:
+				return heuristica.mutar(tipo, poblacion, probMutacion,tamPobl);
+			default:
+				return insercion.mutar(tipo, poblacion, probMutacion,tamPobl);
+			
+		}
+	}
+
+}
