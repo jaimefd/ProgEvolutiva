3b5cdc35f71d50c6de46ad6b63e1b9330ae3bc75
juancar99 <62304103+juancar99@users.noreply.github.com>
2021-04-20 16:51:26 +0200

    Insercion,intercambio,inversion

--- G13P2/src/algoritmoGenetico/mutaciones/intercambio.java
+++ G13P2/src/algoritmoGenetico/mutaciones/intercambio.java
@@ -3,7 +3,7 @@ package algoritmoGenetico.mutaciones;
 import java.util.ArrayList;
 import algoritmoGenetico.individuos.Individuo;
 
-public class insercion {
+public class intercambio {
 		
 		static ArrayList<Individuo<Character>> mutar(int tipo, ArrayList<Individuo<Character>> poblacion,
 				double probMutacion,int tamPobl) {
@@ -15,10 +15,11 @@ public class insercion {
 					int posIni=(int)(Math.random()*poblacion.get(i).getTamTotal());
 					int posFin=(int)(Math.random()*poblacion.get(i).getTamTotal());
 					if(posIni>posFin) {
-						Character aux1;
+						Character aux1,aux2;
 						aux1=poblacion.get(i).getCromosoma().get(posIni);		
-						poblacion.get(i).getCromosoma().add(posFin, aux1);
-						poblacion.get(i).getCromosoma().remove(posIni+1);
+						aux2=poblacion.get(i).getCromosoma().get(posFin);
+						poblacion.get(i).getCromosoma().set(posIni, aux2);
+						poblacion.get(i).getCromosoma().set(posFin, aux1);
 					}
 					
 				}
---
+++ G13P2/src/algoritmoGenetico/mutaciones/inversion.java
@@ -0,0 +1,41 @@
+package algoritmoGenetico.mutaciones;
+
+import java.util.ArrayList;
+
+import algoritmoGenetico.individuos.Individuo;
+
+public class inversion {
+	static ArrayList<Individuo<Character>> mutar(int tipo, ArrayList<Individuo<Character>> poblacion,
+			double probMutacion,int tamPobl) {
+		
+		double prob;
+		ArrayList<Character> nueva=new ArrayList<Character>();
+		for(int i=0;i<tamPobl;i++) {
+			prob=Math.random();
+			if(prob<probMutacion) {
+				int posIni=(int)(Math.random()*poblacion.get(i).getTamTotal());
+				int posFin=(int)(Math.random()*poblacion.get(i).getTamTotal());
+				while(posIni==posFin) {
+					posFin=(int)(Math.random()*poblacion.get(i).getTamTotal());
+				}
+				if(posIni>posFin) {
+					int aux=posIni;
+					posIni=posFin;
+					posFin=aux;
+				}
+				for(int j=0;j<posIni;j++) {
+					nueva.add(poblacion.get(i).getCromosoma().get(j));
+				}
+				for(int x=posFin;x>=posIni;x--) {
+					nueva.add(poblacion.get(i).getCromosoma().get(x));
+				}
+				for(int z=posFin+1;z<26;z++) {
+					nueva.add(poblacion.get(i).getCromosoma().get(z));
+				}
+				poblacion.get(i).setCromosoma(nueva);
+			}
+		}
+		
+		return poblacion;
+	}
+}
